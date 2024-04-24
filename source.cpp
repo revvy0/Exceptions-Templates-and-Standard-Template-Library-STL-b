651d667168055ef5dec92dd45f31725284e4d7db
revvy0 <163015200+revvy0@users.noreply.github.com>
2024-04-24 08:43:40 -0400

    Create source.cpp
    added functions and comments for code needed

---
+++ source.cpp
@@ -0,0 +1,24 @@
+// Paul Shelley
+// 4/24/24
+// CIS 1202.501
+
+#include <iostream>
+using namespace std;
+
+// function for float point types
+template<typeName T>
+T half(T value) {
+  // insert code for returning value
+}
+
+// Function for int types
+int half(int value) {
+  // insert code for returning int value
+}
+
+int main() {
+// insert code for testing functions
+
+
+  return 0;
+}
