38f0ba5928d61e472dd8895119de3bc1670ae6cf
revvy0 <163015200+revvy0@users.noreply.github.com>
2024-04-24 17:16:59 -0400

    Update source.cpp
    added new code for main and for returning values

--- source.cpp
+++ source.cpp
@@ -9,16 +9,24 @@ using namespace std;
 template<typeName T>
 T half(T value) {
   // insert code for returning value
+  return value / static_cast<T>(2);
 }
 
 // Function for int types
 int half(int value) {
   // insert code for returning int value
+  return (value % 2 == 0) ? (value / 2) : ((value + 1) / 2);
 }
 
 int main() {
 // insert code for testing functions
+double a = 7.0;
+float b = 5.0f;
+int c = 3;
 
-
+cout << half(a) << endl;
+cout << half(b) << endl;
+cout << half(c) << endl;
+// mimic code from example to see if it works as it should
   return 0;
 }
